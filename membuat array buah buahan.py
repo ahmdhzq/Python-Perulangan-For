2d27d8c474984a0a0de82a9b79c183613ec8f8c5
Ahmad Haziq Mu'izzaddin Wafiq <107825639+ahmdhzq@users.noreply.github.com>
2022-07-14 13:52:41 +0700

    membuat array buah buahan

---
+++ membuat array buah buahan.py
@@ -0,0 +1,8 @@
+# Membuat array
+buah = ['mangga', 'apel', 'jeruk', 'nanas', 'semangka', 'ceri', 'anggur']
+
+# Melakukan perulangan for
+for buah_buahan in buah:
+    
+    # Mencetak buah-buahan
+    print("Nama buah : ",buah_buahan)
